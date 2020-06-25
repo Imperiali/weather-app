06450a1f7dfed367b19cf47c7aa7892e878b3cef
imperiali <igor.imperiali@gmail.com>
2020-06-25 19:49:43 -0300

    Adding component to deal with other days info

---
+++ src/components/NextDay.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card class="d-flex flex-row " tile>
+    <v-spacer class="col-6"></v-spacer>
+    <div class="col-6">
+      <p class="d-flex justify-start">{{dayTitle}}</p>
+      <p class="d-flex justify-start ma-0">{{temperature}}</p>
+    </div>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    name: "NextDay",
+    props: ['dayTitle', 'temperature']
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,6 +3,9 @@
       <InputArea />
       <TodayWeather :forecast="forecast.today"/>
     </div>
+    <NextDay day-title="Amanhã" :temperature="forecast.tomorrow.main.temp + '°C'"/>
+    <NextDay day-title="Depois de amanhã" :temperature="forecast.afterTomorrow.main.temp + '°C'"/>
+  </v-card>
 </template>
 
 <script>
