6f8147b918f63c97d2e2674b9afd466577966b00
imperiali <igor.imperiali@gmail.com>
2020-06-25 00:07:04 -0300

    Getting the location name

--- src/helpers/Geolocation.js
+++ src/helpers/Geolocation.js
@@ -1,9 +1,26 @@
+import * as axios from "axios";
+
 function getGeolocation(callback) {
-    if (navigator.geolocation) {
-        return navigator.geolocation.getCurrentPosition(callback);
-    } else {
-        return "Geolocation is not supported by this browser.";
-    }
+  if (navigator.geolocation) {
+    return navigator.geolocation.getCurrentPosition(callback);
+  } else {
+    return "Geolocation is not supported by this browser.";
+  }
 }
 
-export default {getGeolocation}
+async function getLocationName({lat, long}) {
+  const KEY = 'c63386b4f77e46de817bdf94f552cddf'
+  let result = ''
+  await axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${lat},${long}&` +
+    `key=${KEY}&language=en`).then(res => {
+    result = {
+      'city': res.data.results[0].components.city,
+      'state_code': res.data.results[0].components.state_code,
+      'contry_code': res.data.results[0].components.contry_code,
+    }
+  })
+
+  return result
+}
+
+export default {getGeolocation, getLocationName}
