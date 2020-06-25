3d9e794bde7712739efd96240fa72813c2e09cb2
imperiali <igor.imperiali@gmail.com>
2020-06-25 19:43:42 -0300

    Return promises... ... Adding request to get coords with local name ... Changing data format

--- src/helpers/Geolocation.js
+++ src/helpers/Geolocation.js
@@ -10,19 +10,36 @@ function getGeolocation(callback) {
   }
 }
 
-async function getLocationName({lat, long}) {
-  const KEY = 'c63386b4f77e46de817bdf94f552cddf'
-  let result = ''
-  await axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${lat},${long}&` +
+function getLocationName({latitude, longitude}) {
+  return axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${latitude},${longitude}&` +
     `key=${KEY}&language=en`).then(res => {
-    result = {
-      'city': res.data.results[0].components.city,
-      'state_code': res.data.results[0].components.state_code,
-      'contry_code': res.data.results[0].components.contry_code,
+    return {
+      name: {
+        city: res.data.results[0].components.city,
+        state_code: res.data.results[0].components.state_code,
+        contry_code: res.data.results[0].components.contry_code,
+        formatted: res.data.results[0].formatted
+      }
     }
   })
-
-  return result
 }
 
-export default {getGeolocation, getLocationName}
+function getLocationCoords(locationName) {
+  return axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${locationName}&` +
+    `key=${KEY}&language=en`).then(res => {
+    return {
+      name:{
+        city: res.data.results[0].components.city || res.data.results[0].components.country,
+        state_code: res.data.results[0].components.state_code || res.data.results[0].components.country_code,
+        contry_code: res.data.results[0].components.contry_code || res.data.results[0].components.continent,
+        formatted: res.data.results[0].formatted
+      },
+      coords:{
+        latitude: res.data.results[0].geometry.lat,
+        longitude: res.data.results[0].geometry.lng,
+      }
+    }
+  })
+}
+
+export default {getGeolocation, getLocationName, getLocationCoords}
