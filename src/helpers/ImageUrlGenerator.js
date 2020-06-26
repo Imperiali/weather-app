4c3a44431263b66b74394b34d2a866031b041855
imperiali <igor.imperiali@gmail.com>
2020-06-26 17:56:57 -0300

    Fixing endpoint

--- src/helpers/ImageUrlGenerator.js
+++ src/helpers/ImageUrlGenerator.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 function getImage() {
-  return axios('http://localhost:3000/bingApi').then(res => {
+  return axios('http://localhost:3000/image').then(res => {
     return res.data
   })
 }
