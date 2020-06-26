0713a545bf76bd6016f4d28e3a0b6dfc5d54d8ba
imperiali <igor.imperiali@gmail.com>
2020-06-26 09:39:58 -0300

    Adding a comp to deal with temperatures business

---
+++ src/components/DayTemp.vue
@@ -0,0 +1,26 @@
+<template>
+  <div @click="tempCelsius = !tempCelsius">
+    <p class="d-flex justify-start">{{dayTitle}}</p>
+    <p class="d-flex justify-start ma-0">
+      {{tempCelsius ? getCelcius(dayOption) : getFahrenheit(dayOption)}}</p>
+  </div>
+</template>
+
+<script>
+  import {mapGetters} from "vuex";
+
+  export default {
+    name: "DayTemp",
+    props: ['dayTitle', 'dayOption'],
+    data:() => ({
+      tempCelsius: true
+    }),
+    computed:{
+      ...mapGetters(['getCelcius', 'getFahrenheit'])
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
