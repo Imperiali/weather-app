9c0c741225a703d3bc2311d211f85fb55f3ff20e
imperiali <igor.imperiali@gmail.com>
2020-06-26 17:53:11 -0300

    Removing calls from third party andpoint... ... Now backend deals with it

--- src/helpers/Geolocation.js
+++ src/helpers/Geolocation.js
@@ -22,35 +22,22 @@ function getLocationInfo(location) {
 }
 
 function getLocationName({latitude, longitude}) {
-  return axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${latitude},${longitude}&` +
-    `key=${KEY}&language=en`).then(res => {
-    return {
-      name: {
-        city: res.data.results[0].components.city,
-        state_code: res.data.results[0].components.state_code,
-        contry_code: res.data.results[0].components.contry_code,
-        formatted: res.data.results[0].formatted
-      }
-    }
-  })
+  return getLocationInfo(latitude + ',' + longitude)
+    .then(res => res )
+    .catch(error => {
+      console.log('Error in Get Location Name', error)
+      return error
+    })
 }
 
 function getLocationCoords(locationName) {
-  return axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${locationName}&` +
-    `key=${KEY}&language=en`).then(res => {
-    return {
-      name:{
-        city: res.data.results[0].components.city || res.data.results[0].components.country,
-        state_code: res.data.results[0].components.state_code || res.data.results[0].components.country_code,
-        contry_code: res.data.results[0].components.contry_code || res.data.results[0].components.continent,
-        formatted: res.data.results[0].formatted
-      },
-      coords:{
-        latitude: res.data.results[0].geometry.lat,
-        longitude: res.data.results[0].geometry.lng,
-      }
-    }
-  })
+  locationName = typeof locationName === 'string' ? locationName : resolveLocalName(locationName)
+  return getLocationInfo(locationName)
+    .then(res => res)
+    .catch(error => {
+      console.log('Error in Get Location Coords', error)
+      return error
+    })
 }
 
 export default {getGeolocation, getLocationName, getLocationCoords}
