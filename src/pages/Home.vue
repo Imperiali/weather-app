f3be47f47a59b078333c862ddec9672071f6193c
imperiali <igor.imperiali@gmail.com>
2020-06-25 23:08:31 -0300

    Style and Feature changes... ... Passing Style by helper ... Changing temp scale by click

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,19 @@
 <template>
-  <v-card class="customCard mx-auto" tile>
+  <v-card class="mx-auto" :style="customCard" tile>
     <div>
-      <InputArea />
-      <TodayWeather :forecast="forecast.today"/>
+      <InputArea/>
+      <TodayWeather :style="forecastBackground.today"
+                    :forecast="forecast.today"
+                    :temperature="tempCelsius ? this.$store.getters.getFahrenheit('today') : this.$store.getters.getCelcius('today')"
+                    @click="changeTempScale()"
+      />
     </div>
-    <NextDay day-title="Amanhã" :temperature="forecast.tomorrow.main.temp + '°C'"/>
-    <NextDay day-title="Depois de amanhã" :temperature="forecast.afterTomorrow.main.temp + '°C'"/>
+    <NextDay :style="forecastBackground.tomorrow" day-title="Amanhã"
+             :temperature="tempCelsius ? this.$store.getters.getFahrenheit('tomorrow') : this.$store.getters.getCelcius('tomorrow')"
+             @click="changeTempScale()"/>
+    <NextDay :style="forecastBackground.afterTomorrow" day-title="Depois de amanhã"
+             :temperature="tempCelsius ? this.$store.getters.getFahrenheit('afterTomorrow') : this.$store.getters.getCelcius('afterTomorrow')"
+             @click="changeTempScale()"/>
   </v-card>
 </template>
 
@@ -14,6 +22,7 @@
   import TodayWeather from "../components/TodayWeather";
   import {mapState} from "vuex";
   import NextDay from "../components/NextDay";
+  import Style from "../helpers/Style";
 
   export default {
     name: "Home",
@@ -22,7 +31,26 @@
       TodayWeather,
       NextDay
     },
-    computed: mapState(['forecast']),
+    data: () => ({
+      customCard: Style.backgroundCustom(),
+      tempCelsius: true,
+      temp: ''
+    }),
+    computed: {
+      ...mapState(['forecast']),
+      forecastBackground() {
+        return {
+          today: Style.imageFilter(this.forecast.today.main.temp),
+          tomorrow: Style.imageFilter(this.forecast.tomorrow.main.temp),
+          afterTomorrow: Style.imageFilter(this.forecast.afterTomorrow.main.temp),
+        }
+      }
+    },
+    methods: {
+      changeTempScale() {
+        this.tempCelsius = !this.tempCelsius
+      }
+    }
   }
 </script>
 
