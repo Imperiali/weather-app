d31c0cc7cb5a663daaf9cdf04b38055bee8cb7fc
imperiali <igor.imperiali@gmail.com>
2020-06-26 17:56:34 -0300

    Simplifing weather help... ... Just making the api call

--- src/helpers/Weather.js
+++ src/helpers/Weather.js
@@ -1,13 +1,20 @@
 import * as axios from "axios";
 
-function getWeather({city, state_code, country_code}) {
+function getWeather(local) {
   const KEY = '7ba73e0eb8efe773ed08bfd0627f07b8'
-  return axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city || ''},${state_code || ''},${country_code | ''}&` +
+  return axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${local}&` +
     `cnt=17&units=metric&lang=pt_br&APPID=${KEY}`).then(res => {
     return {
       today: res.data.list[0],
       tomorrow: res.data.list[8],
       afterTomorrow:res.data.list[16]
+    }
+  }).catch(error => {
+    console.log('Error in Get Weather', error)
+    return {
+      today: '',
+      tomorrow: '',
+      afterTomorrow: ''
     }
   })
 }
