be84a82511798a03d5e28d2f475cef8cbf42e642
imperiali <igor.imperiali@gmail.com>
2020-06-29 20:17:47 -0300

    Adding internationalization with i18n

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import store from './store'
+import i18n from './i18n'
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
