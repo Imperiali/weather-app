ca12009ef8e7cd0a9a0e8f20278514aa096c8d96
imperiali <igor.imperiali@gmail.com>
2020-06-29 16:34:10 -0300

    Using env variables

---
+++ express/config.js
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config({
+  path: process.env.NODE_ENV === "production" ? ".env.production" : ".env.development"
+});
+module.exports = {
+  geocodeKey: process.env.GEOCODE_KEY,
+  weatherKey: process.env.WEATHER_KEY,
+  port: process.env.PORT,
+  path: process.env.PATH,
+};
