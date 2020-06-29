51e3ed755d23edbd2302df4565c5f4aca797f9ab
imperiali <igor.imperiali@gmail.com>
2020-06-29 12:15:44 -0300

    Getting the weather from server

--- src/helpers/Weather.js
+++ src/helpers/Weather.js
@@ -1,21 +1,9 @@
 import * as axios from "axios";
 
 function getWeather(local) {
-  const KEY = '7ba73e0eb8efe773ed08bfd0627f07b8'
-  return axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${local}&` +
-    `cnt=17&units=metric&lang=pt_br&APPID=${KEY}`).then(res => {
-    return {
-      today: res.data.list[0],
-      tomorrow: res.data.list[8],
-      afterTomorrow:res.data.list[16]
-    }
-  }).catch(error => {
+  return axios.get(`http://localhost:3000/weather?location=${local}`).then(res => res.data)
+    .catch(error => {
     console.log('Error in Get Weather', error)
-    return {
-      today: '',
-      tomorrow: '',
-      afterTomorrow: ''
-    }
   })
 }
 
