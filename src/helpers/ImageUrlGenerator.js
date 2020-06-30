4231db6c2854b404c23b3d41c63eb4cca4efcff7
imperiali <igor.imperiali@gmail.com>
2020-06-30 18:35:56 -0300

    Adding catch error to getImage helper... ... And display the error to console

--- src/helpers/ImageUrlGenerator.js
+++ src/helpers/ImageUrlGenerator.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 
 const getImage = () => {
-  return axios(`${process.env.VUE_APP_API_URL}/image`).then(res => {
-    return res.data
-  })
+  return axios(`${process.env.VUE_APP_API_URL}/image`)
+    .then(res => {
+      return res.data
+    }).catch(err => {
+      console.error('There is and error getting image: ', err)
+    })
 }
 
-export default { getImage }
+export default {getImage}
