c675e5315c3b932727ecd6342c7fe2e3a2ceba50
imperiali <igor.imperiali@gmail.com>
2020-07-02 00:48:53 -0300

    Stop passing style... ... Comp now deals with style

--- src/components/DayRow.vue
+++ src/components/DayRow.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-card class="d-flex elevation-0 justify-space-around" :style="forecastBackground" tile>
-    <v-col cols="6" align-self="center" class="d-flex justify-center">
+  <v-card :class="forecastBackground" class="d-flex elevation-0 justify-space-around" tile>
+    <v-col align-self="center" class="d-flex justify-center" cols="6">
       <slot name="left"></slot>
     </v-col>
     <v-col  cols="6">
@@ -21,12 +21,20 @@
       ...mapGetters(['getForecast']),
       forecastBackground() {
         let temp = this.getForecast[this.day] ? this.getForecast[this.day].main.temp : ''
-        return Style.backgroundColorTemp(temp)
+        if (!temp) {
+          return 'default'
+        } else if (temp >= 30) {
+          return 'hot'
+        } else if (temp <= 15) {
+          return 'cold'
+        } else {
+          return 'normal'
+        }
       }
     }
   }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+  @import "src/assets/style";
 </style>
