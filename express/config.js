e6e5af48ca3eb17eb65804fba908c1ed5ac2d176
imperiali <igor.imperiali@gmail.com>
2020-07-17 10:44:41 -0300

    Fixing environment variables from the back-end

--- express/config.js
+++ express/config.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv');
 dotenv.config({
-  path: process.env.NODE_ENV === "production" ? ".env.production" : ".env.development"
+  path: process.env.NODE_ENV === "production" ? ".env.production" : ".env.example"
 });
 module.exports = {
   geocodeKey: process.env.GEOCODE_KEY,
